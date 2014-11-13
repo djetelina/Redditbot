2e936189d0430d50c2d68609a90b45339f1f6e46
iScrE4m <djt@seznam.cz>
2014-11-13 23:00:15 +0100

    added spritesheeter

--- bot.py
+++ bot.py
@@ -7,28 +7,46 @@ import time
 # bot plugins
 import plugins.sidebar as sidebar
 import plugins.stalker as stalker
+import plugins.spritesheeter as spritesheeter
 
 def main():
-	# Initiation for getting data
-	print "Welcome to /r/scrolls bot."
-	username = raw_input("Please enter reddit username: ")
-	password = raw_input("Please enter reddit password: ")
-	print "Thank you for helping me help you help us all. Let's begin."
 
-	schedule.every(5).minutes.do(lambda: sidebar.streams(username, password))
-	schedule.every().hour.do(lambda: sidebar.ladder(username, password))
-	schedule.every().hour.do(lambda: stalker.stalk(username, password))
+	# Choose plugins to run
+	print "\n\nWelcome to /r/scrolls bot. Choose your weapon:\n"
+	print "=============================================="
+	print "1: Sidebar updates and mojangsta stalking."
+	print "2: create emoticon spritesheet."
+	print "==============================================\n"
+	whattodo = raw_input("What will it be?\n")
 
-	# on the startup do them both and THEN start counting time
-	stalker.stalk(username, password)
-	sidebar.streams(username, password)
-	print("Waiting 10s for Reddit to update its description, this will only happen once.")
-	time.sleep(10)
-	sidebar.ladder(username, password)
+	if whattodo == "1":
+		print "You chose to run script that's updating sidebar and stalks mojangstas."
+		username = raw_input("Please enter reddit username: ")
+		password = raw_input("Please enter reddit password: ")
+		print "Thank you for helping me help you help us all. Let's begin."
 
-	while True:
-		schedule.run_pending()
-		time.sleep(1)
+		schedule.every(5).minutes.do(lambda: sidebar.streams(username, password))
+		schedule.every().hour.do(lambda: sidebar.ladder(username, password))
+		schedule.every().hour.do(lambda: stalker.stalk(username, password))
+
+		# on the startup do them both and THEN start counting time
+		stalker.stalk(username, password)
+		sidebar.streams(username, password)
+		print("Waiting 10s for Reddit to update its description, this will only happen once.")
+		time.sleep(10)
+		sidebar.ladder(username, password)
+
+		while True:
+			schedule.run_pending()
+			time.sleep(1)
+
+	elif whattodo == "2":
+		print "You chose to create emoticon spritesheet"
+		spritesheeter.emoti()
+
+	else:
+		print "Let's try that again."
+		main()
 
 if __name__ == '__main__':
     main()
---
+++ plugins/spritesheeter.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import os
+import Image
+
+# All related files are in subfolder \emoti\
+# Use icon_map.txt and enter name_of_emoti:file.png per line. All files must be the same resolution (24x24px in our case).
+
+def emoti():
+
+    script_dir = os.path.dirname(__file__)
+    rel_path = "emoti/icon_map.txt"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    iconMapFile = open(abs_file_path)
+    iconMap = sorted( line.rstrip().split(':') for line in iconMapFile.readlines()
+                      if line.rstrip())
+    iconMapFile.close()
+
+    images = [Image.open(os.path.join(script_dir, "emoti/%s" % (filename))) for cssClass, filename in iconMap]
+
+    print "%d images will be combined." % len(images)
+
+    image_width, image_height = images[0].size
+
+    print "all images assumed to be %d by %d." % (image_width, image_height)
+
+    master_width = image_width
+    master_height = (image_height * len(images))
+    print "the master image will by %d by %d" % (master_width, master_height)
+    print "creating image...",
+    master = Image.new(
+        mode='RGBA',
+        size=(master_width, master_height),
+        color=(0,0,0,0))
+
+    print "created."
+
+    for count, image in enumerate(images):
+        location = image_height*count
+        print "adding %s at %d..." % (iconMap[count][1], location),
+        master.paste(image,(0,location))
+        print "added."
+    print "done adding icons."
+
+    print "saving master.gif...",
+    master.save((os.path.join(script_dir, "emoti/master.gif")), transparency=0 )
+    print "saved!"
+
+    print "saving master.png...",
+    master.save((os.path.join(script_dir, "emoti/master.png")))
+    print "saved!"
+
+
+    cssTemplate = '''a[href="/%s"] {
+        background-position: 0px %dpx;
+    }
+    '''
+
+    for format in ['png','gif']:
+        print 'saving icons_%s.css...' % format,
+        iconCssFile = open((os.path.join(script_dir, "emoti/icons_%s.css" % format )),'w')
+        for count, pair in enumerate(iconMap):
+            cssClass, filename = pair
+            location = image_height*count
+            iconCssFile.write( cssTemplate % (cssClass, location) )
+        iconCssFile.close()
+        print 'created!'
+
+if __name__ == '__main__':
+    emoti()
